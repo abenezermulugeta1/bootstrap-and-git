f96f823aa2b6c6f5f72d9802fcfe2f963e60741a
Abenzer Mulugeta <abenezermulugeta6020@gmail.com>
2020-10-28 11:32:29 +0300

    video play pause button added and some chages on video,and join now button style updated

--- asset/js/script.js
+++ asset/js/script.js
@@ -9,3 +9,12 @@ function openNav() {
     document.getElementById("main").style.marginLeft= "0";
     document.body.style.backgroundColor = "white";
   }
+  $('.video').parent().click(function () {
+    if($(this).children(".video").get(0).paused){
+        $(this).children(".video").get(0).play();
+        $(this).children(".playpause").fadeOut();
+    }else{
+       $(this).children(".video").get(0).pause();
+        $(this).children(".playpause").fadeIn();
+    }
+});
